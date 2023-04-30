62914183a1e3aa7c9b4e3a0a79847c011f8acbae
krishna sahu <krishnasahu000537@gmail.com>
2023-04-30 21:31:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Resturant from "./components/Basics/Resturant";
+function App() {
+  return (
+    <div>
+      <Resturant/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Basics/menuApi.js
@@ -0,0 +1,83 @@
+// import React from "react";
+
+const Menu = [{
+    id:'1',
+    image:"images/maggi.jpg",
+    name:"maggi",
+    category:"breakfast",
+    price:"20$",
+    description: "Maggi introduced extensive social benefits that were unusual for the time, such as a canteen, workers' housing, company health insurance, widow's and old-age pensions, and in 1906 Saturdays off. "
+},
+{
+    id:'2',
+    image:"images/allupakoida.jpg",
+    name:"allupakoida",
+    category:"lunch",
+    price:"40$",
+    description: "lorem epson the content creator"
+},
+{
+    id:'3',
+    image:"images/chola.jpg",
+    name:"chola",
+    category:"lunch",
+    price:"45$",
+    description: "lorem epson the content creator"
+},
+{
+    id:'4',
+    image:"images/corn.jpg",
+    name:"corn",
+    category:"breakfast",
+    price:"80$",
+    description: "lorem epson the content creator"
+},
+{
+    id:'5',
+    image:"images/momo.jpg",
+    name:"momo",
+    category:"evening",
+    price:"80$",
+    description: "lorem epson the content creator"
+},
+{
+    id:'6',
+    image:"images/nonvegthali.jpg",
+    name:"nonvegthali",
+    category:"dinner",
+    price:"10$",
+    description: "lorem lore epson the content creator"
+},
+{
+    id:'7',
+    image:"images/pizza.jpg",
+    name:"pizza",
+    category:"evening",
+    price:"70$",
+    description: "lorem epson the content creator"
+},{
+    id:'8',
+    image:"images/puri.jpg",
+    name:"puri",
+    category:"dinner",
+    price:"20$",
+    description: "lorem epson the content creator"
+},
+{
+    id:'9',
+    image:"images/rajmarice.jpg",
+    name:"rajmarice",
+    category:"dinner",
+    price:"20$",
+    description: "lorem epson the content creator"
+}
+,
+{
+    id:'10',
+    image:"images/rajmarice.jpg",
+    name:"daalchawal",
+    category:"kabhibhi",
+    price:"20$",
+    description: "lorem epson the content creator"
+}]
+export default Menu;
